2e9200b24b884b49e9a33f41df16fab5b5240300
alka31 <belajarfathan@gmail.com>
2026-05-26 11:51:05 +0700

    Chore: final text updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>🚀 GOKIL! Aplikasi Web Docker Berhasil Didistribusikan via CI/CD Pipeline LKS Modul 2! 🚀</h1>');
+  res.send('<h1>🚀 AKHIRNYAA! Aplikasi Web Docker Berhasil Didistribusikan via CI/CD Pipeline LKS Modul 2! 🚀</h1>');
 });
 
 app.listen(PORT, () => {
